c89385d12d14090d23bebfed468648aff9f236db
siddharthapd <sid.cse2013@gmail.com>
2024-03-10 15:09:41 +0530

    Refactored and added default scope

--- deployment/src/main/java/com/wasdshinobi/processor/ConfigBuildProcessor.java
+++ deployment/src/main/java/com/wasdshinobi/processor/ConfigBuildProcessor.java
@@ -7,10 +7,11 @@ import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.deployment.IsDevelopment;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
-import io.quarkus.deployment.builditem.FeatureBuildItem;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.function.BooleanSupplier;
+
+import static io.quarkus.arc.processor.DotNames.SINGLETON;
 
 @Slf4j
 public class ConfigBuildProcessor {
@@ -20,7 +21,8 @@ public class ConfigBuildProcessor {
         log.info("Adding Dev Mode Beans...");
         var builder = AdditionalBeanBuildItem.builder().setUnremovable();
 
-        builder.addBeanClass(DevModeBean.class);
+        builder.addBeanClass(DevModeBean.class)
+                .setDefaultScope(SINGLETON);
 
         additionalBeans.produce(builder.build());
     }
@@ -30,7 +32,8 @@ public class ConfigBuildProcessor {
         log.info("Adding Prod Mode Beans...");
         var builder = AdditionalBeanBuildItem.builder().setUnremovable();
 
-        builder.addBeanClass(ProdModeBean.class);
+        builder.addBeanClass(ProdModeBean.class)
+                .setDefaultScope(SINGLETON);
 
         additionalBeans.produce(builder.build());
     }
